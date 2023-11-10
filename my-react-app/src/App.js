fa7c373752a5f91c3f4159b9cdccb010c2983b21
harshraj6582 <harshraj211430@gmail.com>
2023-11-10 09:14:02 +0530

    Part1

---
+++ my-react-app/src/App.js
@@ -0,0 +1,33 @@
+import {  useSelector } from 'react-redux/es/hooks/useSelector';
+import { useDispatch } from 'react-redux';
+import './App.css';
+
+
+function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({type:'INC'});
+
+  }
+  const decrement = () => {
+    dispatch({type:'DEC'});
+  }
+  const addBy = () => {
+    dispatch({type : 'ADD' , payload : 10  })
+  }
+  
+  return (
+    <div >
+    <h1>Counter App</h1>
+      <h2>{counter}</h2>
+
+
+      <button onClick={increment}>Increement</button>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={addBy}>Add By Value  10 </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/store/index.js
@@ -0,0 +1,23 @@
+import {createStore} from 'redux';
+
+const reducerFn = (state =  {counter : 0}, action) =>{
+
+
+
+    // Synchronous Function 
+    // We should not Mutate the Original State 
+
+    if(action.type === "INC"){
+        return {counter: state.counter + 1};
+    }
+    if(action.type === "DEC"){
+        return {counter: state.counter -  1};
+    }
+
+    if(action.type === "ADD"){
+        return {counter: state.counter +  action.payload};
+    }
+    return state ; 
+}
+const store = createStore(reducerFn)
+export default store ; 
